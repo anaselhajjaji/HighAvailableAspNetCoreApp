50d7f1533477bbcad9b9e74d96d3880cb7ec24c8
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-03-02 10:18:05 -0500

    add health check publisher

---
+++ HealthChecks/HealthCheckPublisher.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace SystemdHealthcheck.HealthChecks
+{
+    public class HealthCheckPublisher : IHealthCheckPublisher
+    {
+        private readonly ILogger _logger;
+
+        public HealthCheckPublisher(ILogger<HealthCheckPublisher> logger)
+        {
+            _logger = logger;
+        }
+
+        // The following example is for demonstration purposes only. Health Checks 
+        // Middleware already logs health checks results. A real-world readiness 
+        // check in a production app might perform a set of more expensive or 
+        // time-consuming checks to determine if other resources are responding 
+        // properly.
+        public Task PublishAsync(HealthReport report, 
+            CancellationToken cancellationToken)
+        {
+            if (report.Status == HealthStatus.Healthy)
+            {
+                _logger.LogInformation("{Timestamp} Readiness Probe Status: {Result}", 
+                    DateTime.UtcNow, report.Status);
+            }
+            else
+            {
+                _logger.LogError("{Timestamp} Readiness Probe Status: {Result}", 
+                    DateTime.UtcNow, report.Status);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SystemdHealthcheck.HealthChecks;
 
 namespace SystemdHealthcheck
 {
@@ -25,6 +26,17 @@ namespace SystemdHealthcheck
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Add Health Check publisher
+            services.AddHealthChecks();
+
+            services.Configure<HealthCheckPublisherOptions>(options =>
+            {
+                options.Delay = TimeSpan.FromSeconds(2);
+                options.Predicate = (check) => check.Tags.Contains("ready");
+            });
+
+            services.AddSingleton<IHealthCheckPublisher, HealthCheckPublisher>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +54,7 @@ namespace SystemdHealthcheck
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
