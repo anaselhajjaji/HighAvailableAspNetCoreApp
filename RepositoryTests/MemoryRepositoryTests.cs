d80ca787ddaa6da3b816e5f33eaeb70a7ab0a445
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-03-05 13:52:40 -0500

    add unit test

--- RepositoryTests/MemoryRepositoryTests.cs
+++ RepositoryTests/MemoryRepositoryTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Repository.Tests
 {
@@ -12,13 +13,15 @@ namespace Repository.Tests
         [TestMethod()]
         public void InsertTest()
         {
-            Assert.Fail();
-        }
+            MemoryRepository repository = new MemoryRepository();
 
-        [TestMethod()]
-        public void GetAllTest()
-        {
-            Assert.Fail();
+            Assert.AreEqual(repository.GetAll().Count(), 0);
+
+            repository.Insert(new Models.Employee { Id = 1, FirstName = "Employee 1", LastName = "Employee 1", Email = "Email 1" });
+            Assert.AreEqual(repository.GetAll().Count(), 1);
+
+            repository.Insert(new Models.Employee { Id = 2, FirstName = "Employee 2", LastName = "Employee 2", Email = "Email 2" });
+            Assert.AreEqual(repository.GetAll().Count(), 2);
         }
     }
 }
